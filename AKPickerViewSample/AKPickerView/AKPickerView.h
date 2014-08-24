b537fe888a1b3fa2257ad80987f7983c0f9d1533
Akkyie Y <akio@prioris.org>
2014-08-25 03:12:36 +0900

    Update AKPickerView.h
    Fix trifling mistake

--- AKPickerViewSample/AKPickerView/AKPickerView.h
+++ AKPickerViewSample/AKPickerView/AKPickerView.h
@@ -25,8 +25,8 @@
 @property (nonatomic, strong) UIFont *highlightedFont;
 @property (nonatomic, strong) UIColor *textColor;
 @property (nonatomic, strong) UIColor *highlightedTextColor;
-@property (nonatomic, assign) CGFloat interitemSpacing; // 0...1; slight value recommended such as 0.0001
-@property (nonatomic, assign) CGFloat fisheyeFactor;
+@property (nonatomic, assign) CGFloat interitemSpacing;
+@property (nonatomic, assign) CGFloat fisheyeFactor; // 0...1; slight value recommended such as 0.0001
 @property (nonatomic, assign, readonly) NSUInteger selectedItem;
 
 - (void)reloadData;
